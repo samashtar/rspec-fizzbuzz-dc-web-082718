f06abff6b5f2eb942dcd8fd2f18d6994aff630b1
Sam Ashtar <samashtarm@gmail.com>
2018-08-06 00:08:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,14 +4,16 @@
 
 
 def fizzbuzz(int) 
-  if int % 3 === 0
+  
+   if (num % 5 == 0) && (num % 3 == 0)
+    "FizzBuzz"
+  elsif int % 3 === 0
     "Fizz"
 
    elsif int % 5 === 0
     "Buzz"
   
-     elsif (num % 5 == 0) && (num % 3 == 0)
-    "FizzBuzz"
+    
   end
 
 end
